4702a02661c533e37a51d5c56f503d86ab1a0b3e
eru123 <yeoligoakino@gmail.com>
2022-03-05 19:03:16 +0800

    added ParseQueryError Exception

---
+++ src/lib/Errors.js
@@ -0,0 +1,17 @@
+class ParseQueryError extends Error {
+  constructor() {
+    super("Please follow format: 'domain.tld' or 'domain'");
+    this.code = "DOMAIN_CHECKER_PARSE_QUERY_ERROR"
+    this.name = "DomainCheckerParseQueryError"
+    
+    // remove the default stack trace
+    this.stack =
+      this.stack.split('\n').splice(0, 1).join('\n') +
+      "\n" +
+      this.stack.split('\n').slice(3).join('\n');
+  }
+}
+
+module.exports = {
+  ParseQueryError
+}
