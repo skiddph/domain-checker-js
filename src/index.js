c7890776b36a260a97906ec671973db777b21274
eru123 <yeoligoakino@gmail.com>
2022-03-08 03:05:26 +0800

    update vendors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import freenom from './vendor/freenom.js'
 import parseQuery from './lib/parseQuery.js'
+import freenom from './vendor/freenom.js'
+import namesilo from './vendor/namesilo.js'
+import google from './vendor/google.js'
 
 export const methods = {
   freenom,
+  namesilo,
+  google
 }
 
 export const vendors = Object.keys(methods)
@@ -11,7 +15,7 @@ export const domainChecker = (query) => {
   const { domain, tld } = parseQuery(query);
   const res = {}
   for (let vendor of vendors) {
-    res[ vendor ] = async () => await methods[ vendor ]({domain, tld})
+    res[ vendor ] = async (opts) => await methods[ vendor ]({domain, tld, ...opts})
   }
   return res
 }
